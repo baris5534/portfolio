54b0b81d578e75b8056b97c3cda8b0bb7790764f
Ferhat-kus <55ferhat.kus@gmail.com>
2024-11-25 17:13:46 +0300

    chore: aspect radio image size edited

--- src/components/Profileimg.jsx
+++ src/components/Profileimg.jsx
@@ -1,19 +1,23 @@
 import profile from "../assets/profilepp.jpg";
-import {motion} from "motion/react"
-function Profileimg ()  {
+import { motion } from "motion/react";
+
+function Profileimg() {
   return (
     <motion.div
-    initial={{opacity: 0, scale:0}}  
-    animate={{opacity: 1, scale: 1}}
-    transition={{duration: 0.5}}
-    
-    className="rounded-full my-3 border-4 select-none pointer-events-none border-white shadow-white shadow-2xl">
-    <div className="border-2 border-black rounded-full ">
-      <img
-      src={profile}
-      alt="profile"
-      className="size-[500px] max-sm:size-[280px] max-[360px]:size-[250px] border-4  rounded-full object-cover"
-    /></div></motion.div>
-  )
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}
+      className="rounded-full my-3 border-4 select-none pointer-events-none border-white shadow-white shadow-2xl"
+    >
+      <div className="border-2 border-black rounded-full size-[500px] max-sm:size-[280px] max-[360px]:size-[250px] lg:aspect-radio aspect-square">
+        <img
+          src={profile}
+          alt="profile"
+          className="w-full h-full rounded-full object-cover"
+        />
+      </div>
+    </motion.div>
+  );
 }
+
 export default Profileimg;
