8503ae1d28de761827c3826be33bef2cba2b7315
baris5534 <samsuncrsmb55@gmail.com>
2025-02-18 14:46:58 +0300

    arka plan gitti

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       
       <div className="relative z-10">
         <main className="container bg-white/0 min-h-screen mx-auto flex flex-col items-start justify-center">
-          <section className="flex flex-col max-xl:flex-col lg:flex-col lg:h-[900px] max-2xl:h-[700px] max-sm:w-full xl:h-screen items-center justify-around h-screen max-sm:h-scree max-xl:h-[750px] w-full py-10 px-10" >
+          <section className="flex flex-col max-xl:flex-col lg:flex-col lg:h-[900px] max-2xl:h-[700px] max-sm:w-full xl:h-screen items-center justify-around h-screen max-sm:h-scree max-xl:h-[750px] w-full py-6 px-10" >
             <Profileimg/>
             <article className="flex w-6/12 max-xl:w-full">
               <div className="flex flex-col">
@@ -29,11 +29,11 @@ function App() {
           <Suspense fallback={<div className="w-full h-32 flex items-center justify-center">
             <div className="animate-pulse text-white/50">Yükleniyor...</div>
           </div>}>
-            <section className="w-full py-6">
+            <section className="w-full py-5">
               <Frameworks/>
             </section>
             
-            <section className="w-full py-5">
+            <section className="w-full py-3">
               <Projects/>
             </section>
           </Suspense>
